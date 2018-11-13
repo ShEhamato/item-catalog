0d60937963e94de52aeff8a9f13aeb640e8ab7b3
shereenhamato <shereen.ahmed@wavelet.net>
2018-11-13 11:37:41 +0800

    feat: add url for catalog edit

--- application.py
+++ application.py
@@ -8,9 +8,16 @@ app = Flask(__name__)
 def listCatalog():
     return render_template('catalog-list.html')
 
+
 @app.route('/catalogs/<int:catalog_id>/view')
 def viewCatalog(catalog_id):
-    return render_template('catalog-view.html')    
+    return render_template('catalog-view.html') 
+
+@app.route('/catalogs/<int:catalog_id>/edit')
+def editCatalog(catalog_id):
+    return render_template('catalog-edit.html')  
+
+    
 if __name__=='__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
