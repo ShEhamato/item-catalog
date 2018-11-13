a64ca242805b78f508490c317bcdec7590acef84
shereenhamato <shereen.ahmed@wavelet.net>
2018-11-13 11:26:57 +0800

    feat: add url for catalog view

--- application.py
+++ application.py
@@ -7,7 +7,10 @@ app = Flask(__name__)
 @app.route('/catalogs')
 def listCatalog():
     return render_template('catalog-list.html')
-    
+
+@app.route('/catalogs/<int:catalog_id>/view')
+def viewCatalog(catalog_id):
+    return render_template('catalog-view.html')    
 if __name__=='__main__':
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
