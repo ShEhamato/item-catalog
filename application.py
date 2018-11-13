d35ca93c27704903206bb972959e6516b3228a37
shereenhamato <shereen.ahmed@wavelet.net>
2018-11-13 17:51:54 +0800

    feat: add post to html forms

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 
 catalogList=[{'id':1,'name':'Sports','description':'Items that relate to sports including'},
 {'id':2,'name':'Technology','description':'Items that relate to high technology including'},
@@ -7,15 +7,19 @@ catalogList=[{'id':1,'name':'Sports','description':'Items that relate to sports 
 app = Flask(__name__)
 
 
+
 @app.route('/')
 @app.route('/catalogs')
 def listCatalog():
     return render_template('catalog-list.html', catalog_list=catalogList )
 
 
-@app.route('/catalogs/new')
+@app.route('/catalogs/new', methods=['POST', 'GET'])
 def createCatalog():
-    return render_template('catalog-new.html')  
+    if request.method == 'POST':
+        return redirect(url_for('listCatalog'))
+    else:
+        return render_template('catalog-new.html')  
 
 
 @app.route('/catalogs/<int:catalog_id>/view')
@@ -23,14 +27,20 @@ def viewCatalog(catalog_id):
     return render_template('catalog-view.html', catalog = catalogList[catalog_id-1] ) 
 
 
-@app.route('/catalogs/<int:catalog_id>/edit')
+@app.route('/catalogs/<int:catalog_id>/edit', methods=['POST', 'GET'])
 def editCatalog(catalog_id):
-    return render_template('catalog-edit.html' , catalog = catalogList[catalog_id-1] ) 
+    if request.method == 'POST':
+        return redirect(url_for('listCatalog'))
+    else:
+        return render_template('catalog-edit.html' , catalog = catalogList[catalog_id-1] ) 
 
 
-@app.route('/catalogs/<int:catalog_id>/delete')
+@app.route('/catalogs/<int:catalog_id>/delete' , methods=['POST', 'GET'])
 def deleteCatalog(catalog_id):
-    return render_template('catalog-delete.html', catalog = catalogList[catalog_id-1] ) 
+    if request.method == 'POST':
+        return redirect(url_for('listCatalog'))
+    else:
+        return render_template('catalog-delete.html', catalog = catalogList[catalog_id-1] ) 
 
 
 
