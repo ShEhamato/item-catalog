ca9f557b1d0fc195c6751fa33fd1499d88f2b0b1
shereenhamato <shereen.hamato@gmail.com>
2018-11-14 12:25:16 +0800

    feat: add user table

--- database_setup.py
+++ database_setup.py
@@ -6,12 +6,24 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+class User(Base):
+    __tablename__ = 'user'
+
+    id= Column(Integer, primary_key= True)
+    name=Column(String(80), nullable=False)
+    email=Column(String(80), nullable=False)
+    picture = Column(String(250))
+
 class Catalog(Base):
     __tablename__ = 'catalog'
 
     id = Column(Integer, primary_key =True)
     name = Column(String(250) , nullable = False)
     description = Column(String(250))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    
+    
 
 class CatalogItem(Base):
     __tablename__ = 'catalog_item'
@@ -23,6 +35,8 @@ class CatalogItem(Base):
     duration = Column(String(250))
     catalog_id = Column(Integer, ForeignKey('catalog.id'))
     catalog = relationship(Catalog)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
 
 
 engine = create_engine('sqlite:///catalog.db')
