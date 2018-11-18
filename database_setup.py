8fe341c76d45d0662fca5d29afc566de0b42b46d
shereenhamato <shereen.hamato@gmail.com>
2018-11-19 07:31:44 +0800

    fix: apply code styling

--- database_setup.py
+++ database_setup.py
@@ -1,24 +1,26 @@
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship ,  backref
+from sqlalchemy.orm import relationship,  backref
 from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class User(Base):
     __tablename__ = 'user'
 
-    id= Column(Integer, primary_key= True)
-    name=Column(String(80), nullable=False)
-    email=Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(80), nullable=False)
+    email = Column(String(80), nullable=False)
     picture = Column(String(250))
+
 
 class Catalog(Base):
     __tablename__ = 'catalog'
 
-    id = Column(Integer, primary_key =True)
-    name = Column(String(250) , nullable = False)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
     description = Column(String(250))
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
@@ -28,11 +30,10 @@ class Catalog(Base):
         """Return object data in easily serializeable format"""
         return {
             'name': self.name,
-            'description':self.description,
+            'description': self.description,
             'id': self.id,
         }
-    
-    
+
 
 class CatalogItem(Base):
     __tablename__ = 'catalog_item'
@@ -43,7 +44,10 @@ class CatalogItem(Base):
     price = Column(String(8))
     duration = Column(String(250))
     catalog_id = Column(Integer, ForeignKey('catalog.id'))
-    catalog = relationship(Catalog, backref=backref("items", cascade="all,delete"))
+    catalog = relationship(
+        Catalog,
+        backref=backref("items", cascade="all,delete")
+        )
     user_id = Column(Integer, ForeignKey('user.id'))
     user = relationship(User)
 
@@ -56,12 +60,9 @@ class CatalogItem(Base):
             'id': self.id,
             'price': self.price,
             'duration': self.duration,
-        
         }
-    
 
 
 engine = create_engine('sqlite:///catalog.db')
-
 
 Base.metadata.create_all(engine)
